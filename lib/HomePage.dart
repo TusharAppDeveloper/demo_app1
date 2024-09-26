cf2c6cda710bb4260d4314a57d94af941209552a
bdCalling <engkhukan@gmail.com>
2024-09-26 08:43:11 +0600

    new

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [Container()],
       ),
     );
   }
